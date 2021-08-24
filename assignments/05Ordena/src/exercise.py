a2329bc08e9feccbbdd7cf6e3cd2bbb95f4fa23d
NicoleConde <88851171+NicoleConde@users.noreply.github.com>
2021-08-23 23:08:29 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,7 +1,20 @@
-def main():
-    # Escribe el código adecuado para completar el programa
-    pass
-
-
-if __name__=='__main__':
-    main()
+print("Enter the first number: ")
+num1=int(input())
+print("Enter the second number: ")
+num2=int(input())
+print("Enter the third number: ")
+num3=int(input())
+if(num1<num2 and num2<num3) :
+    print("",num1,"-", num2, "-",num3)
+elif(num2<num1 and num1<num3) :
+    print("",num2,"-", num1, "-",num3)
+elif(num3<num1 and num1<num2) :
+    print("",num3,"-", num1, "-",num2)
+elif(num3<num2 and num2<num1) :
+    print("",num3,"-", num2, "-",num1)
+elif(num1<num3 and num3<num2) :
+    print("",num1,"-", num3, "-",num2)
+elif(num2<num3 and num3<num1) :
+    print("",num2,"-", num3, "-",num1)
+else:
+    print("They are equal numbers.")
